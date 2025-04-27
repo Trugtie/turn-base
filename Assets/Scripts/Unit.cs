ff13866583e0385a882ee194f6d7891bc1ac18ba
Won <58035906+Trugtie@users.noreply.github.com>
2025-04-27 16:25:18 +0700

    feat: rotate when move

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -2,7 +2,16 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+    private const string IS_WALKING = "IsWalking";
+
+    [SerializeField] private Animator _animator;
+
     private Vector3 _targetPosition;
+
+    private void Awake()
+    {
+        _animator = GetComponentInChildren<Animator>();
+    }
 
     private void Update()
     {
@@ -13,11 +22,17 @@ public class Unit : MonoBehaviour
 
         float stopThresoldDistance = 0.1f;
         float moveDistance = Vector3.Distance(transform.position, _targetPosition);
+        _animator.SetBool(IS_WALKING, false);
         if (moveDistance < stopThresoldDistance) return;
 
         float speed = 4f;
         Vector3 moveDirection = (_targetPosition - transform.position).normalized;
         transform.position += speed * Time.deltaTime * moveDirection;
+
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
+
+        _animator.SetBool(IS_WALKING, true);
     }
 
     public void SetTargetPosition(Vector3 targetPosition)
