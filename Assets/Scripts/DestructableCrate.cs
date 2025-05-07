fe74dc04717d7b2a4e364bffb0e41e62f2cf39d4
Won <58035906+Trugtie@users.noreply.github.com>
2025-05-07 17:13:49 +0700

    feat: add dectructive create

--- Assets/Scripts/DestructableCrate.cs
+++ Assets/Scripts/DestructableCrate.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 
 public class DestructableCrate : MonoBehaviour
 {
+    [SerializeField] private Transform _createDestroyedPrefab;
     public static event EventHandler OnAnyDestructableCrateDamge;
-    private GridPosition _gridPosition;
 
+    private GridPosition _gridPosition;
     private void Awake()
     {
         _gridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
@@ -16,6 +17,18 @@ public class DestructableCrate : MonoBehaviour
     public void Damge()
     {
         Destroy(gameObject);
+        Transform createDestroyedInstance = Instantiate(_createDestroyedPrefab, transform.position, Quaternion.identity);
+        ApplyExplosionToCrateDestoyed(createDestroyedInstance, 150f, transform.position, 10f);
         OnAnyDestructableCrateDamge?.Invoke(this, EventArgs.Empty);
+    }
+
+    private void ApplyExplosionToCrateDestoyed(Transform rootBone, float explosionForce, Vector3 explosionPosition, float explosionRange)
+    {
+        foreach (Transform child in rootBone)
+        {
+            if (!child.TryGetComponent(out Rigidbody childRigidbody)) continue;
+            childRigidbody.AddExplosionForce(explosionForce, explosionPosition, explosionRange);
+            ApplyExplosionToCrateDestoyed(child, explosionForce, explosionPosition, explosionRange);
+        }
     }
 }
